5725f0f397a91715c17ea415298083f43bd7904b
Vlagle <75324088+Vlagle@users.noreply.github.com>
2021-11-13 17:03:53 +0500

    1

--- index.php
+++ index.php
@@ -1,42 +1,38 @@
-<?php include('con.php');?>
 <html>
-<head>
-	<title>File uploader</title>
-	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
-</head>
-
-<body>
-
-<?php
-if($_GET['upload'])
-		{
-			echo '<center><form action="upload.php" method="post" enctype="multipart/form-data">';
-			echo 'Ник: <input type="text" name="nick" size="40" />&nbsp;&nbsp;<br>';
-			echo 'Описание: <input type="text" name="desc" size="40" />&nbsp;&nbsp;<br>';
-			echo 'Файл: <input type="file" name="FILE" size="40" />&nbsp;&nbsp;<br>';
-			echo '<input type="submit" value="Пиу">';
-			echo '</form></center>';
-		}
-	else
-		{
-			echo "<center><a href=\"/?upload=1\">новый файл</a></center>";
-
-			 $result = mysql_query("SELECT `id`,`nick`,`desc` FROM `hostf` ORDER BY id DESC",$db);
-			 if (mysql_num_rows($result) > 0)
-	{
-		echo "<center><table border=\"1\"><tr><td>id</td><td>nick</td><td colspan=\"2\">desc</td></tr>";
-		while ($myrow = mysql_fetch_array($result))
+	<head>
+		<title><?=$_SERVER ["HTTP_HOST"];?>: One click file hostion</title>
+		<link rel="stylesheet" type="text/css" media="all" href="style.css">
+	</head>
+	<body>
+		<table width="100%" height="100%"><tr valign="center"><td align="center" nowrap><div id="upload">
+		<?php
+			if (@$_POST ['upload'] == "")
+			{
+				echo 'Select file to upload | Maximum file size 2000 MB | Split archives allowed<br><br>';
+				echo '<form enctype="multipart/form-data" action="" method="post">';
+				echo '<input type="hidden" name="MAX_FILE_SIZE" value="30000">';
+				echo '<input type="file" name="file" size="45">';
+				echo '<input type="submit" value=" Upload " name="upload">';
+				echo '</form>';
+			} else {
+				$upload_dir = "/uploads/";
+				$upload_path = dirname (__FILE__).$upload_dir;
+				$upload_filename = md5 (microtime());
+				$upload_link = "http://".$_SERVER ["HTTP_HOST"].dirname ($_SERVER ["PHP_SELF"]).$upload_dir.$upload_filename;
+				if (@move_uploaded_file ($_FILES['file']['tmp_name'], $upload_path.$upload_filename))
 				{
-	      printf("\n<tr><td><a href=\"file.php?id=%s\">%s</a></td><td>%s</td><td>%s</td><td><a href=\"file.php?id=%s\">Скачать</a></td></tr>\n",
-	      $myrow["id"],$myrow["id"],$myrow["nick"],$myrow["desc"],$myrow["id"]);
-		};
-
-		echo"</table></center>";
-	}
-
-		}
-?>
-
-</body>
-
+					echo "<b>File is valid, and was successfully uploaded.</b><br><br>";
+					echo "Download Link:<br>";
+					echo "<input type='text' size=80 onclick='this.select()' value='".$upload_link."'><br><br>";
+					echo "Download Link in HTML (for use in web sites, myspace, blogs, etc):<br>";
+					echo "<input type='text' size=80 onclick='this.select()' value=\"<a href='".$upload_link."'>".$upload_link."</a>\"><br><br>";
+					echo "Download Link in Forum code (for use in phpBB, vBulletin, etc):<br>";
+					echo "<input type='text' size=80 onclick='this.select()' value='[url]".$upload_link."[/url]'><br><br>";
+					echo "<a href='?".md5(microtime())."'>Upload another file</a>";
+				} else {
+					echo "<b>There some errors!</b>";
+				}
+			}
+		?>
+	</div></td></tr></table></body>
 </html>
